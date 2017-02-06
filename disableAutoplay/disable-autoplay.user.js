271582f97a7de26066f282b4cc39693a3149cb65
Andrey Meshkov <ay.meshkov@gmail.com>
2017-02-06 19:06:15 +0300

    Fix metacritic.comhttps://github.com/AdguardTeam/Userscripts/issues/9

--- disableAutoplay/disable-autoplay.user.js
+++ disableAutoplay/disable-autoplay.user.js
@@ -1,18 +1,52 @@
 // ==UserScript==
 // @name         Disable Video AutoPlay by Adguard
 // @namespace    https://adguard.com/
-// @version      1.0.1
+// @version      1.0.3
 // @description  Ensures that HTML5 video elements do not autoplay
 // @author       Adguard
 // @include      http://*
 // @include      https://*
-// @run-at       document-end
+// @run-at       document-start
 // @downloadURL  https://github.com/AdguardTeam/Userscripts/raw/master/disableAutoplay/disable-autoplay.user.js
 // @updateURL    https://github.com/AdguardTeam/Userscripts/raw/master/disableAutoplay/disable-autoplay.user.js
 // @grant        GM_info
+// @grant        GM_log
+// @grant        window
 // ==/UserScript==
 
 (function() {
+
+	/**
+	 * Metacritic.com
+	 * https://github.com/AdguardTeam/Userscripts/issues/9
+	 */
+	var metacritic = function() {
+        if (!window.localStorage) {
+        	// Do not handle old browsers
+            return;
+        }
+
+        window.localStorage.setItem("mc_autoplay", "false");
+	};
+    
+    /**
+     * For some hosts it is not enough to use a generic rule.
+     * So we have to use complicated rules instead.
+     */
+    var hostRules = {
+    	"www.metacritic.com": metacritic
+    };
+
+    /**
+     * Applies per-host rules
+     */
+    var applyHostRules = function(){
+        var hostRule = hostRules[window.location.hostname];
+
+        if (hostRule) {
+            hostRule.apply(this);
+        }
+    };
     
     /**
      * Searches for all "video" elements and changes "autoplay" attribute to false
@@ -42,15 +76,20 @@
      * Initialize script
      */
 	var init = function() {
-       
-        // Check existing elements first
-        removeAutoplay();
 
-        // Set up a mutation observer to handle dynamically added video tags
-        var observer = new MutationObserver(handleDomChange);
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
+        // Apply per-host rule
+        applyHostRules();
+       
+        window.addEventListener("DOMContentLoaded", function() {
+            // Check existing elements first
+            removeAutoplay();
+
+            // Set up a mutation observer to handle dynamically added video tags
+            var observer = new MutationObserver(handleDomChange);
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
         });
 	};
 
