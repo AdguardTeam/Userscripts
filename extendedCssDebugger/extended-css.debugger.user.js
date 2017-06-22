7a377890742a9406ec029e8126b4bdda462c512c
Andrey Meshkov <am@adguard.com>
2017-06-22 10:48:34 +0300

    Add extcss debugger

---
+++ extendedCssDebugger/extended-css.debugger.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Extended Css Debugger
+// @namespace    https://adguard.com/
+// @version      0.1
+// @description  A very simple userscript that exposes ExtendedCss object. Use `ExtendedCss.query` calls to debug selectors.
+// @author       Adguard
+// @require      https://AdguardTeam.github.io/ExtendedCss/extended-css.js
+// @match        http://*/*
+// @match        https://*/*
+// @grant        unsafeWindow
+// @downloadURL  https://github.com/AdguardTeam/Userscripts/raw/master/extendedCssDebugger/extended-css.debugger.user.js
+// @updateURL    https://github.com/AdguardTeam/Userscripts/raw/master/extendedCssDebugger/extended-css.debugger.user.js
+// ==/UserScript==
+
+unsafeWindow.ExtendedCss = ExtendedCss;
